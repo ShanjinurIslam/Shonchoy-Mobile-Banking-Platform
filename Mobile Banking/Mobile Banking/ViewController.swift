87f7dfad2a959cbdc48f7f5f5ccdded9d95dd9f9
ShanjinurIslam <spondoncsebuet@gmail.com>
2020-06-01 08:57:40 +0600

    ios project init

--- Mobile Banking/Mobile Banking/ViewController.swift
+++ Mobile Banking/Mobile Banking/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+           // Do any additional setup after loading the view.
+        activityIndicator.startAnimating()
     }
-
-
 }
 
