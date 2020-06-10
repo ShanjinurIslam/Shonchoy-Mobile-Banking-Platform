bf1d7dd745024cfef57cf39726ece8d0718e0af0
ShanjinurIslam <spondoncsebuet@gmail.com>
2020-06-10 21:33:21 +0600

    photo works

--- shonchoy/lib/view/take_photo.dart
+++ shonchoy/lib/view/take_photo.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'package:image/image.dart' as DartImage;
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +21,7 @@ class TakePhotoState extends State<TakePhoto> {
   bool showCapturedPhoto = false;
   var imagePath;
 
-  void onCaptureButtonPressed() async {
+  void onCaptureButtonPressed(GlobalKey key) async {
     //on camera button press
     try {
       final path = join(
@@ -30,6 +30,14 @@ class TakePhotoState extends State<TakePhoto> {
       );
       imagePath = path;
       await _controller.takePicture(path); //take photo
+
+      RenderBox box = key.currentContext.findRenderObject();
+      Offset position = box.localToGlobal(Offset.zero);
+      var image = DartImage.decodeImage(File(imagePath).readAsBytesSync());
+      var rotate = DartImage.copyRotate(image, 90);
+      var cropped = DartImage.copyCrop(
+          rotate, position.dx.toInt(), position.dy.toInt(), 700, 500);
+      File(imagePath)..writeAsBytesSync(DartImage.encodePng(cropped));
 
       setState(() {
         showCapturedPhoto = true;
@@ -68,10 +76,48 @@ class TakePhotoState extends State<TakePhoto> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final deviceRatio = size.width / size.height;
-
+    GlobalKey key = GlobalKey();
     return Scaffold(
+      backgroundColor: Colors.black,
       body: showCapturedPhoto
-          ? Center(child: Image.file(File(imagePath)))
+          ? SafeArea(
+              child: Center(
+                  child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Spacer(),
+                Center(
+                  child: Image.file(
+                    File(imagePath),
+                  ),
+                ),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    FlatButton(
+                        onPressed: () {
+                          setState(() {
+                            showCapturedPhoto = false;
+                          });
+                        },
+                        child: Text(
+                          'Retake',
+                          style: TextStyle(color: Colors.white),
+                        )),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Next',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )))
           : FutureBuilder<void>(
               future: _initializeControllerFuture,
               builder: (context, snapshot) {
@@ -85,6 +131,36 @@ class TakePhotoState extends State<TakePhoto> {
                           child: Stack(
                             children: <Widget>[
                               CameraPreview(_controller),
+                              Align(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    Container(
+                                      key: key,
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                          color: Colors.grey,
+                                          width:
+                                              1, //                   <--- border width here
+                                        ),
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                      height: 200,
+                                      width: MediaQuery.of(context).size.width *
+                                          .75,
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(10),
+                                      child: Text(
+                                        'Place ID inside this frame',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                alignment: Alignment.center,
+                              ),
                               Padding(
                                 padding: EdgeInsets.all(24),
                                 child: Align(
@@ -93,7 +169,7 @@ class TakePhotoState extends State<TakePhoto> {
                                           borderRadius:
                                               BorderRadius.circular(10)),
                                       onPressed: () {
-                                        onCaptureButtonPressed();
+                                        onCaptureButtonPressed(key);
                                       },
                                       child: Icon(CupertinoIcons.photo_camera)),
                                   alignment: Alignment.bottomCenter,
